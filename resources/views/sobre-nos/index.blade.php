e321512431368ee8c91f6e4e1108952e06cff8b7
Vinicius Melchior <vini.melchior.vm@gmail.com>
2021-06-15 00:04:40 -0300

    modifica sobre-nos

--- resources/views/sobre-nos/index.blade.php
+++ resources/views/sobre-nos/index.blade.php
@@ -6,4 +6,25 @@
     <p>Fundado em 2020, o Intituto Hope é uma organização social sem fins lucrativos, que atua na cidade de São Paulo, com projetos que tem fundamento de garantir a defesa dos direitos de crianças e adolescentes, com foco na prevenção e no enfrentamento da violência, e na luta pela sobrevivência de famílias em situação de rua e vulnerabilidade social, tendo também projetos voltados contra a violência e abandono dos animais. Juntos, movemos o mundo para proteger os animais, seguindo os princípios e diretrizes desta política. Nosso trabalho, complementa a ação desenvolvida por serviços públicos ou filantrópicos em diferentes áreas, como por exemplo a área de acolhimento e amparo social.</p>
     <h4>"O ato de doar significa ajudar o próximo, faze-lo feliz de alguma forma, por mais simples que seka sua ação. A cada ajuda, um mérito próprio."</h4>
 </main>
+
+
+<section class="equipe">
+
+    <h2>Equipe</h2>
+
+    <div class="equipe-content">
+        <div class="img-content">
+            <img src="imagens/dev-paulo.jfif" alt="">
+            <p class="info-cargo">Paulo Henrique - Desenvolvedor Front-End</p>
+        </div>
+        <div class="img-content">
+            <img src="imagens/dev-lucas.jfif" alt="">
+            <p class="info-cargo">Lucas Arthur - Desenvolvedor Front-End</p>
+        </div>
+        <div class="img-content">
+            <img src="imagens/vini-dev.jpg" alt="">
+            <p class="info-cargo">Vinicius Melchior - Desenvolvedor Back-End</p>
+        </div>
+    </div>
+</section>
 @endsection
